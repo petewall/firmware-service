f03ac35cb783621d23c6956fb2b72ef20d91f5ed
Pete Wall <pete.wall@grafana.com>
2023-02-20 10:24:42 -0600

    Add helper functions here, for use in other projects

--- lib/firmware.go
+++ lib/firmware.go
@@ -1,9 +1,20 @@
 package lib
 
+import (
+	"github.com/Masterminds/semver"
+	"sort"
+)
+
 type Firmware struct {
 	Type    string `json:"type"`
 	Version string `json:"version"`
 	Size    int64  `json:"size"`
+}
+
+func (f *Firmware) IsOlderThan(otherFirmware *Firmware) bool {
+	version, _ := semver.NewVersion(f.Version)
+	otherVersion, _ := semver.NewVersion(otherFirmware.Version)
+	return version.LessThan(otherVersion)
 }
 
 type FirmwareList []*Firmware
@@ -20,4 +31,33 @@ func (fl FirmwareList) GetUniqueTypes() []string {
 	}
 
 	return types
+}
+
+func (fl FirmwareList) GetLatest(includingPrerelease bool) *Firmware {
+	list := fl
+	if !includingPrerelease {
+		list = fl.FilterOutPrerelease()
+	}
+	if len(list) == 0 {
+		return nil
+	}
+	list.Sort()
+	return list[len(list)-1]
+}
+
+func (fl FirmwareList) Sort() {
+	sort.Slice(fl, func(a, b int) bool {
+		return fl[a].IsOlderThan(fl[b])
+	})
+}
+
+func (fl FirmwareList) FilterOutPrerelease() FirmwareList {
+	var filtered FirmwareList
+	for _, firmware := range fl {
+		version, _ := semver.NewVersion(firmware.Version)
+		if version.Prerelease() == "" {
+			filtered = append(filtered, firmware)
+		}
+	}
+	return filtered
 }
